b51c3edb0a75768ac86addb0317ab19c84091966
Duong Tang <dttang1994@gmail.com>
2024-06-06 02:26:41 -0700

    got numbers to display on webpage.

--- javascript.js
+++ javascript.js
@@ -5,8 +5,28 @@ let a = 0;
 let this_result = 0;
 
 const container = document.querySelector(".container");
-const numbers = document.querySelector(".input");
-const operators = document.querySelector(".operator");
+const numbers = document.querySelectorAll(".input");
+const operators = document.querySelectorAll(".operator");
 const display = document.querySelector(".display");
 const clear = document.querySelector(".clear");
 const equals = document.querySelector(".equals");
+
+const add = (a,b) => a+b;
+const subtract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => a/b;
+
+const displayInput = (input) => display.textContent = input;
+
+function getNumbers(event){
+    if(operator===""){
+        constant_one += event.target.textContent;
+        displayInput(constant_one);
+    }
+    else{
+        constant_two += event.target.textContent;
+        displayInput(constant_two);
+    }
+}
+
+numbers.forEach((element) => element.addEventListener("click", getNumbers));
