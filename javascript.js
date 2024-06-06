c314aeca0cd5aca3e8c88fb6075767634c75ab9b
Duong Tang <dttang1994@gmail.com>
2024-06-06 02:06:34 -0700

    added buttons and dom objects.

---
+++ javascript.js
@@ -0,0 +1,12 @@
+let constant_one = "";
+let constant_two = "";
+let operator = "";
+let a = 0;
+let this_result = 0;
+
+const container = document.querySelector(".container");
+const numbers = document.querySelector(".input");
+const operators = document.querySelector(".operator");
+const display = document.querySelector(".display");
+const clear = document.querySelector(".clear");
+const equals = document.querySelector(".equals");
